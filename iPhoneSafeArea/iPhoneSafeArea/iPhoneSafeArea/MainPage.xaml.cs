fc007260417a783f5420180a305349455a5f8382
Jorge DevsDNA <jorge@devsdna.com>
2019-11-20 19:59:44 +0100

    iPhone safe area example

---
+++ iPhoneSafeArea/iPhoneSafeArea/iPhoneSafeArea/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace iPhoneSafeArea
+{
+    using Xamarin.Forms;
+    using Xamarin.Forms.PlatformConfiguration;
+    using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            On<iOS>().SetUseSafeArea(true);
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            //var safeInsets = On<iOS>().SafeAreaInsets();
+            //safeInsets.Left = 80;
+            //Padding = safeInsets;
+        }
+    }
+}
